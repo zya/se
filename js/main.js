37c21515a7cf709463e76e8ecb92686f258d5f72
Ehsan Ziya <ehsan.ziya@gmail.com>
2014-12-18 01:06:52 +0000

    audio added - analysis and vertex displacement

--- js/main.js
+++ js/main.js
@@ -1,13 +1,36 @@
+//--------------------- audio global vars ----------------------//
+window.AudioContext = (window.AudioContext || window.webkitAudioContext || window.mozAudioContext || window.msAudioContext);
+var context = new AudioContext();
+var analyser = context.createAnalyser();
+analyser.fftSize = 1024;
+analyser.smoothingTimeConstant = 0.7;
+var filter = context.createBiquadFilter();
+filter.type = 'highpass';
+filter.frequency.value = 4000;
+var audiosource = context.createBufferSource();
+audiosource.connect(filter);
+filter.connect(analyser);
+audiosource.connect(context.destination);
+var frequencyData = new Uint8Array(analyser.fftSize);
+var isloaded = false;
+//--------------------- three global vars ----------------------//
 var renderer, scene, camera, controls;
 var mesh, geometry, material, texture;
+var vertices =[], originalvertices = [];
+var offset1, offset2, offset3;
 var ambient = 0x000000, 
 	diffuse = 0x000000, 
 	specular = 0x000000, 
-	shininess = 0.0, 
+	shininess = 0.0,
 	scale = 50;
 var uniforms;
 var vs, fs;
 
+//--------------------- helper methods ----------------------//
+var map = function(value, istart, istop, ostart, ostop) {
+	return ostart + (ostop - ostart) * ((value - istart) / (istop - istart));
+};
+//--------------------- setup ----------------------//
 function setup() {
 	renderer = new THREE.WebGLRenderer({
 		antialias: true
@@ -23,20 +46,27 @@ function setup() {
 
 	controls = new THREE.OrbitControls(camera);
 	controls.minDistance = 200;
-	controls.maxDistance = 500;
+	controls.maxDistance = 700;
 	controls.noKeys = true;
-	controls.maxPolarAngle = 1.8;
-	controls.minPolarAngle = 1.2;
+	controls.maxPolarAngle = 2.2;
+	controls.minPolarAngle = 0.8;
+	controls.maxAzimuthAngle = 1.3;
+	controls.minAzimuthAngle = -1.3;
 	controls.noPan = true;
 	controls.autoRotate = true;
-	controls.autoRotateSpeed = 0.2;
+	controls.autoRotateSpeed = 0.25;
 	controls.zoomSpeed = 0.2;
+	controls.rotateUp(-0.2);
+	controls.rotateLeft(-0.2);
 
-	geometry = new THREE.PlaneBufferGeometry(200,200,70,70);
+	geometry = new THREE.PlaneBufferGeometry(200,200,100,100);
+    geometry.dynamic = true;
 	geometry.computeTangents();
+
 
 	var manager = new THREE.LoadingManager();
 	var loader = new THREE.XHRLoader(manager);
+    var audioLoader = new THREE.XHRLoader(manager);
 
 	loader.load('shaders/vs.glsl', function(e){
 		vs = e;
@@ -46,21 +76,29 @@ function setup() {
 		fs = e;
 	});
 
+    audioLoader.setResponseType("arraybuffer");
+    audioLoader.load("audio/1.mp3",function(e){
+        context.decodeAudioData(e, function(buffer){
+           audiosource.buffer = buffer;
+           audiosource.start(0);
+        });
+    });
+
 	texture = new THREE.ImageUtils.loadTexture('images/1.png');
 
 	var shader = THREE.ShaderLib.normalmap;
 	uniforms = THREE.UniformsUtils.clone(shader.uniforms);
 	uniforms[ "enableDisplacement" ].value = true;
-  uniforms[ "enableDiffuse" ].value = 0;
-  uniforms[ "tDiffuse" ].value = texture;
-  uniforms[ "tDiffuseOpacity" ] = { type: 'f', value: 1.0 };
-  uniforms[ "tDisplacement" ] = { type: 't', value: texture};
-  uniforms[ "uDisplacementScale" ].value = 100;
-  uniforms[ "ambientLightColor" ].value = new THREE.Color( ambient );
-  uniforms[ "uDisplacementPostScale" ] = {type: 'f', value: scale };
-  uniforms[ "diffuse" ].value = new THREE.Color( diffuse );
-  uniforms[ "specular" ].value = new THREE.Color( specular );
-  uniforms[ "ambient" ].value = new THREE.Color( ambient );
+    uniforms[ "enableDiffuse" ].value = 0;
+    uniforms[ "tDiffuse" ].value = texture;
+    uniforms[ "tDiffuseOpacity" ] = { type: 'f', value: 1.0 };
+    uniforms[ "tDisplacement" ] = { type: 't', value: texture};
+    uniforms[ "uDisplacementScale" ].value = 100;
+    uniforms[ "ambientLightColor" ].value = new THREE.Color( ambient );
+    uniforms[ "uDisplacementPostScale" ] = {type: 'f', value: scale };
+    uniforms[ "diffuse" ].value = new THREE.Color( diffuse );
+    uniforms[ "specular" ].value = new THREE.Color( specular );
+    uniforms[ "ambient" ].value = new THREE.Color( ambient );
     uniforms[ "shininess" ].value = shininess;
 	
 	manager.onLoad = function(){
@@ -72,10 +110,45 @@ function setup() {
             wireframe: true
     	});
 		mesh = new THREE.Mesh(geometry, material);
+
+        vertices = mesh.geometry.getAttribute("position").array;
+        for(var i = 0; i < vertices.length; i++){
+            originalvertices.push(vertices[i]);
+        }
+
+        offset1 = Math.ceil(vertices.length / 8);
+        offset2 = Math.ceil(vertices.length / 1.5);
+        offset3 = Math.ceil(vertices.length / 2.5);
+
+        isloaded = true;
 		scene.add(mesh);
 	};
 }
+//--------------------- drawing methods ----------------------//
+function analyseAudio(){
+	analyser.getByteFrequencyData(frequencyData);
+}
 
+function updateVertices(){
+
+	if(isloaded) {
+        var sum = 0;
+        for(var i = 0; i < frequencyData.length; i++){
+            sum += frequencyData[i];
+            var index = Math.ceil(map(i, 0, frequencyData.length, 0, vertices.length));
+            var value = map(frequencyData[i], 0, 256, 1, 1.2);
+            vertices[index + offset1] = originalvertices[index + offset1] * value;
+            vertices[index + offset2] = originalvertices[index + offset2] * value;
+            vertices[index + offset3] = originalvertices[index + offset3] * value;
+        }
+        var average = sum/frequencyData.length;
+        if( average > 20) {
+            scale = average + 50;
+        }
+        geometry.addAttribute( 'position', new THREE.BufferAttribute( vertices, 3 ) );
+    }
+}
+//--------------------- event listeners ----------------------//
 function setupListeners(){
 	window.addEventListener('resize', function(){
 		renderer.setSize( window.innerWidth, window.innerHeight );
@@ -84,9 +157,12 @@ function setupListeners(){
 	}, false);
 }
 
+//--------------------- draw ----------------------//
 function draw() {
-	uniforms[ "uDisplacementPostScale" ].value = scale;
+	analyseAudio();
+	updateVertices();
 	controls.update();
+	uniforms[ "uDisplacementPostScale" ].value = scale;
 	renderer.render(scene, camera);
 	requestAnimationFrame(draw);
 }
